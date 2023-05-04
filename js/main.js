6ef3dbd4ffac2a7d72f717fa8e258d05d86ddc48
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-05-04 10:26:42 -0300

    Melhorando o código para listar todos os botões do robo

--- js/main.js
+++ js/main.js
@@ -2,18 +2,58 @@ const somar = document.querySelector('#somar');
 const subtrair = document.querySelector('#subtrair');
 const braco = document.querySelector('#braco');
 
-somar.addEventListener('click', () => {
- manipulaDados('somar');
-});
+const controle = document.querySelectorAll('.controle-ajuste');
 
-subtrair.addEventListener('click', () => {
-  manipulaDados('subtrair'); 
-});
- 
-function manipulaDados(operacao) {
-if(operacao === 'somar'){
-  braco.value = parseInt(braco.value) + 1
-}else{
-  braco.value = parseInt(braco.value) - 1
+
+controle.forEach( (elemento) => {
+  elemento.addEventListener('click', (evento) => {
+    manipulaDados(evento.target.textContent);
+  })
+})
+
+function manipulaDados(manipulado) {
+  if(manipulado === '+') {
+    braco.value = parseInt(braco.value) + 1;
+  }else{
+    braco.value = parseInt(braco.value) - 1
+  }
 }
- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const somar = document.querySelector('#somar');
+// const subtrair = document.querySelector('#subtrair');
+// const braco = document.querySelector('#braco');
+
+// const controle = document.querySelectorAll('.controle-ajuste');
+
+// controle.forEach( (elemento) => {
+//   elemento.addEventListener('click', (evento) => {
+//     manipulaDados(evento.target.textContent);
+//   })
+// })
+
+ 
+// function manipulaDados(operacao) {
+// if(operacao === '+'){
+//   braco.value = parseInt(braco.value) + 1
+// }else{
+//   braco.value = parseInt(braco.value) - 1
+// }
+//  }
