2db1583053735e4553556bfa15f6a6c34254799e
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2023-05-09 19:38:32 -0300

    Aplicando conceitos de data-attributes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const controle = document.querySelectorAll('.controle-ajuste');
+const controle = document.querySelectorAll('[data-controle]');
 
 controle.forEach( (elemento) => {
   elemento.addEventListener('click', (evento) => {
-    manipulaDados(evento.target.textContent, evento.target.parentNode);
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
   })
 })
 
 function manipulaDados(manipulado, controle) {
-  const peca = controle.querySelector('.controle-contador');
+  const peca = controle.querySelector('[data-contador]');
   
   if(manipulado === '+') {
     peca.value = parseInt(peca.value) + 1;
   }else{
-    peca.value = parseInt(braco.value) - 1
+    peca.value = parseInt(peca.value) - 1;
   }
 }
 
@@ -32,25 +32,3 @@ function manipulaDados(manipulado, controle) {
 
 
 
-
-
-// const somar = document.querySelector('#somar');
-// const subtrair = document.querySelector('#subtrair');
-// const peca = document.querySelector('#peca');
-
-// const controle = document.querySelectorAll('.controle-ajuste');
-
-// controle.forEach( (elemento) => {
-//   elemento.addEventListener('click', (evento) => {
-//     manipulaDados(evento.target.textContent);
-//   })
-// })
-
- 
-// function manipulaDados(operacao) {
-// if(operacao === '+'){
-//   peca.value = parseInt(peca.value) + 1
-// }else{
-//   peca.value = parseInt(peca.value) - 1
-// }
-//  }
